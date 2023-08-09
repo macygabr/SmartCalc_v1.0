41f225339787f58fced854475c6910902d4e40ba
Macy Gabrielle <macygabr@in-f1.nsk.21-school.ru>
2023-08-09 11:55:26 +0700

    start_01

--- src/s21_smartCalc.h
+++ src/s21_smartCalc.h
@@ -1,8 +1,5 @@
 #ifndef SRC_S21_SMARTCALC_H_
 #define SRC_S21_SMARTCALC_H_
 #include <stdio.h>
-double s21_add(double value_1, double value_2);
-double s21_sub(double value_1, double value_2);
-double s21_mul(double value_1, double value_2);
-double s21_div(double value_1, double value_2);
+`
 #endif
