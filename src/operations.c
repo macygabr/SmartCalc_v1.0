e114e5d7d230c881c37a85dba358c7e81059994d
Macy Gabrielle <macygabr@ob-h4.nsk.21-school.ru>
2023-08-09 11:45:24 +0700

    start

---
+++ src/operations.c
@@ -0,0 +1,22 @@
+#include"s21"
+
+
+double s21_add(double value_1, double value_2){
+    
+    return 0;
+}
+
+double s21_sub(double value_1, double value_2){
+    
+    return 0;
+}
+
+double s21_mul(double value_1, double value_2){
+    
+    return 0;
+}
+
+double s21_div(double value_1, double value_2){
+    
+    return 0;
+}
