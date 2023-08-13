0bff05febf6a2ad547fdc92086b40800ea1f757f
Macy Gabrielle <macygabr@ob-h4.nsk.21-school.ru>
2023-08-13 21:00:37 +0700

    polish parser

--- src/calc.c
+++ src/calc.c
@@ -1,49 +1,116 @@
-#include"calc.h"
-#include <string.h>
+#include "calc.h"
+
 #include <stdlib.h>
+#include <string.h>
 
-int calc(char* content){
-    DblLinkedList* list =  createDblLinkedList();
-    convert(content, list);
-
-    return 0;
+double calc(char* content) {
+  double res = 0.0;
+  char input[256];
+  convert(content, input);
+  for (int i = 0; i < (int)strlen(content); i++) content[i] = input[i];
+  polish(content);
+  res = arithmetic(content);
+  return res;
 }
+int polish(char* content) {
+  char steck[256];
+  char res[256];
+  int j = 0;
+  int last = 0;
 
-DblLinkedList* createDblLinkedList() {
-    DblLinkedList *tmp = (DblLinkedList*) malloc(sizeof(DblLinkedList));
-    tmp->size = 0;
-    tmp->head = tmp->tail = NULL;
-    return tmp;
-}
-
-Node* pushBack(DblLinkedList *list, char value) {
-    Node *tmp = (Node*) malloc(sizeof(Node));
-    tmp->value = value;
-    tmp->mass = mass(value);
-    tmp->next = NULL;
-    tmp->prev = list->tail;
-    list->size++;
-    return tmp;
-}   
-
-Node* popBack(DblLinkedList *list) {
-    Node *next;
-    char tmp;
- 
-    next = list->tail;
-    list->tail = list->tail->prev;
-    if (list->tail) {
-        list->tail->next = NULL;
+  for (int i = 0; content[i] != '\0'; i++) {
+    if ((content[i] <= '9' && content[i] >= '0') || content[i] == '.') {
+      res[j++] = content[i];
+      if (add_space(content[i + 1])) res[j++] = ' ';
     }
-    if (next == list->head) {
-        list->head = NULL;
+    if (content[i] == '(' || operation(content[i]))
+      add_steck(steck, content[i], &last);
+    if (operator(content[i])) {
+      while (mass(steck[last - 1]) >= mass(content[i])) {
+        last--;
+        res[j++] = steck[last];
+        res[j++] = ' ';
+      }
+      add_steck(steck, content[i], &last);
     }
-    tmp = next->value;
-    free(next);
-    list->size--;
-    return next;
+    if (content[i] == ')') {
+      while (steck[last - 1] != '(') {
+        last--;
+        res[j++] = steck[last];
+        res[j++] = ' ';
+      }
+      steck[last--] = '\0';
+    }
+  }
+
+  while (last > 0) {
+    last--;
+    res[j++] = steck[last];
+    res[j++] = ' ';
+  }
+  res[j - 1] = '\0';
+
+  int k = 0;
+  for (k = 0; res[k] != '\0'; k++) content[k] = res[k];
+  content[k] = '\0';
+  return 0;
 }
 
+double arithmetic(char* content) {
+  for(int i=0; content[i] != ' ' ;i++){
+    
+  }
+  double res = 0.0;
+  return res;
+}
+// _______________________________________support_______________________________________
+int convert(char* content, char* str) {
+  int j = 0;
+  for (int i = 0; i < (int)strlen(content); i++, j++) {
+    if (content[i] == 'c' && content[i + 1] == 'o' && content[i + 2] == 's') {
+      str[j] = 'c';
+      i += 2;
+    } else if (content[i] == 's' && content[i + 1] == 'i' &&
+               content[i + 2] == 'n') {
+      str[j] = 's';
+      i += 2;
+    } else if (content[i] == 't' && content[i + 1] == 'a' &&
+               content[i + 2] == 'n') {
+      str[j] = 't';
+      i += 2;
+    } else if (content[i] == 'l' && content[i + 1] == 'o' &&
+               content[i + 2] == 'g') {
+      str[j] = 'L';
+      i += 2;
+    } else if (content[i] == 'm' && content[i + 1] == 'o' &&
+               content[i + 2] == 'd') {
+      str[j] = 'm';
+      i += 2;
+    } else if (content[i] == 'a' && content[i + 1] == 'c' &&
+               content[i + 2] == 'o' && content[i + 3] == 's') {
+      str[j] = 'C';
+      i += 3;
+    } else if (content[i] == 'a' && content[i + 1] == 's' &&
+               content[i + 2] == 'i' && content[i + 3] == 'n') {
+      str[j] = 'S';
+      i += 3;
+    } else if (content[i] == 'a' && content[i + 1] == 't' &&
+               content[i + 2] == 'a' && content[i + 3] == 'n') {
+      str[j] = 'T';
+      i += 3;
+    } else if (content[i] == 's' && content[i + 1] == 'q' &&
+               content[i + 2] == 'r' && content[i + 3] == 't') {
+      str[j] = 'q';
+      i += 3;
+    } else if (content[i] == 'l' && content[i + 1] == 'n') {
+      str[j] = 'l';
+      i++;
+    } else
+      str[j] = content[i];
+  }
+  str[j] = '\0';
+  return 0;
+}
 int mass(char sign) {
   int res = 0;
   switch (sign) {
@@ -56,50 +123,54 @@ int mass(char sign) {
       break;
     case '*':
     case '/':
+    case 'm':
       res = 3;
       break;
-    default:
+    case 's':
+    case 'c':
+    case 't':
+    case 'S':
+    case 'C':
+    case 'T':
+    case 'q':
+    case 'l':
+    case 'L':
+      res = 3;
+      break;
+    case '^':
       res = 4;
+      break;
+    default:
+      res = 0;
       break;
   }
   return res;
 }
 
-int operation(char content){
-    return content == '*' || content == '/' ||content == '+'||content == '-' ? 1:0;
+int operator(char content) {
+  return content == '^' || content == '*' || content == '/' || content == '+' ||
+                 content == '-' || content == 'm'
+             ? 1
+             : 0;
 }
-int convert(char* content, DblLinkedList* list){
-    int j=0;
-    Node* tmp;
-        tmp = malloc(sizeof(DblLinkedList));
-    for(int i=0; i< strlen(content); i++) {
-        // Число - добавляем в строку вывода.
-        if(content[i] <= '9' && content[i] >= '0') content[j++] = content[i];
-        // Функция или открывающая скобка - помещаем в стек.
-        if(content[i] == '(') pushBack(list, content[i]);
-        // Оператор (O1):
-        
-        if(operation(content[i])){
-            while (tmp->mass > mass(content[i])){
-                content[j++] = tmp->value;
-                popBack(list);
-            }
-            
-           pushBack(list, content[i]);
-           list->tail = tmp;
-        }
-    }
 
-// tmp = tmp->next;
-
-tmp = list->tail;
-printf("[%c]\n", tmp->value);
-
-while(list->size){
-    // content[j++] = tmp->value;
-    // popBack(list);
-    list->size--;
+int operation(char content) {
+  return content == 's' || content == 'c' || content == 't' || content == 'S' ||
+                 content == 'C' || content == 'T' || content == 'q' ||
+                 content == 'l' || content == 'L'
+             ? 1
+             : 0;
 }
-content[j] = '\0';
-return 0;
+int add_steck(char* steck, char content, int* last) {
+  steck[(*last)++] = content;
+  steck[(*last)] = '\0';
+  // printf("steck = [%s]\n", steck);
+  return 0;
+}
+
+int add_space(char content) {
+  if (content == '.' || (content <= '9' && content >= '0'))
+    return 0;
+  else
+    return 1;
 }
--- src/calc.h
+++ src/calc.h
@@ -2,26 +2,13 @@
 #define SRC_S21_SMARTCALC_H_
 #include <stdio.h>
 
-typedef struct _Node {
-	char value;
-    int mass;
-	struct _Node *next;
-	struct _Node *prev;
-} Node;
-
-
-typedef struct _DblLinkedList {
-    size_t size;
-    Node *head;
-    Node *tail;
-} DblLinkedList;
-
-
-int calc(char* content);
+double calc(char* content);
+double arithmetic(char* content);
 int mass(char sign);
-DblLinkedList* createDblLinkedList() ;
-Node* pushBack(DblLinkedList *list, char value);
-Node* popBack(DblLinkedList *list);
-int convert(char* content, DblLinkedList* list);
+int polish(char* content);
+int add_space(char content);
+int convert(char* content, char* str);
+int operator(char content);
 int operation(char content);
+int add_steck(char* steck, char content, int* last);
 #endif
